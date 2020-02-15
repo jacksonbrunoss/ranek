4a227f983b2b25f51178f3a7d7eec6b94a845818
jacksonbrunoss <oi.jbss@gmail.com>
2020-02-15 10:23:14 -0300

    Home

---
+++ src/components/ProdutosBuscar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="buscar-produtos"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="lista-produtos">Lista</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
