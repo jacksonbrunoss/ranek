0990649752c29c34e2d86c9fec02f5b8815eea73
jacksonbrunoss <oi.jbss@gmail.com>
2020-02-20 10:15:00 -0300

    Usuario formulario

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -1,12 +1,44 @@
 <template>
-  <section class="LoginCriar"></section>
+  <section class="LoginCriar">
+    <h3>Crie a sua conta</h3>
+    <transition mode="out-in">
+      <button v-if="!criar" class="btn" @click="criar = true">Criar Conta</button>
+      <UsuarioForm v-else>
+        <button class="btn btn-form">Criar Usuario</button>
+      </UsuarioForm>
+    </transition>
+  </section>
 </template>
 
 <script>
+import UsuarioForm from "./UsuarioForm.vue";
 export default {
-  name: "LoginCriar"
+  name: "LoginCriar",
+  components: {
+    UsuarioForm
+  },
+  data() {
+    return {
+      criar: false
+    };
+  }
 };
 </script>
 
-<style>
+<style scoped>
+h3 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.btn-form {
+  max-width: 100%;
+}
 </style>
---
+++ src/components/UsuarioForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="usuarioForm">
+    <form class="usuario-form">
+      <label for="nome">Nome</label>
+      <input type="name" name="nome" v-model="nome" />
+      <label for="email">Email</label>
+      <input type="email" name="email" v-model="email" />
+      <label for="senha">Senha</label>
+      <input type="password" name="senha" v-model="senha" />
+      <label for="cep">Cep</label>
+      <input type="text" name="cep" v-model="cep" />
+      <label for="rua">Rua</label>
+      <input type="text" name="rua" v-model="rua" />
+      <label for="numero">Numero</label>
+      <input type="text" name="numero" v-model="numero" />
+      <label for="bairro">Bairro</label>
+      <input type="text" name="bairro" v-model="bairro" />
+      <label for="cidade">Cidade</label>
+      <input type="text" name="cidade" v-model="cidade" />
+      <label for="estado">Estado</label>
+      <input type="text" name="estado" v-model="estado" />
+      <div class="button">
+        <slot></slot>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "UsuarioForm"
+};
+</script>
+
+<style scoped>
+.usuario-form {
+  display: grid;
+  grid-template-columns: 80px 1fr;
+  align-items: center;
+}
+.button {
+  grid-column: 2;
+  margin-top: 10px;
+}
+</style>
