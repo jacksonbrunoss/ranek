256092b991ff82b8f15df699b58d9a8a8a33f3e7
jacksonbrunoss <oi.jbss@gmail.com>
2020-02-21 11:10:52 -0300

    Usuario router

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,17 @@ import Home from "../views/Home.vue";
 import Produto from "../views/Produto.vue";
 import Login from "../views/Login.vue";
 import Usuario from "../views/usuario/Usuario.vue";
+import UsuarioProdutos from "../views/usuario/UsuarioProdutos.vue";
+import UsuarioCompras from "../views/usuario/UsuarioCompras.vue";
+import UsuarioEditar from "../views/usuario/UsuarioEditar.vue";
+import UsuarioVendas from "../views/usuario/UsuarioVendas.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home
   },
   {
@@ -26,8 +30,29 @@ const routes = [
   },
   {
     path: "/usuario",
-    name: "usuario",
-    component: Usuario
+    component: Usuario,
+    children: [
+      {
+        path: "",
+        name: "usuario",
+        component: UsuarioProdutos
+      },
+      {
+        path: "compras",
+        name: "compras",
+        component: UsuarioCompras
+      },
+      {
+        path: "editar",
+        name: "usuario-editar",
+        component: UsuarioEditar
+      },
+      {
+        path: "vendas",
+        name: "vendas",
+        component: UsuarioVendas
+      }
+    ]
   }
 ];
 
--- src/views/usuario/Usuario.vue
+++ src/views/usuario/Usuario.vue
@@ -1,10 +1,39 @@
 <template>
-  <p>Usuario: {{$store.state.usuario}}</p>
+  <section class="container">
+    <nav class="sidbar">
+      <ul>
+        <li>
+          <router-link :to="{name: 'usuario'}">Produtos</router-link>
+        </li>
+        <li>
+          <router-link :to="{name: 'compras'}">Compras</router-link>
+        </li>
+        <li>
+          <router-link :to="{name: 'vendas'}">Vendas</router-link>
+        </li>
+        <li>
+          <router-link :to="{name: 'usuario-editar'}">Editar usuario</router-link>
+        </li>
+        <li>
+          <button class="btn" @click.prevent="deslogar">Deslogar</button>
+        </li>
+      </ul>
+    </nav>
+    <transition mode="out-in">
+      <router-view />
+    </transition>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "Usuario"
+  name: "Usuario",
+  methods: {
+    deslogar() {
+      this.$store.dispatch("deslogarUsuario");
+      this.$router.push("/login");
+    }
+  }
 };
 </script>
 
